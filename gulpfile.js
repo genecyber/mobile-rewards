55dd46a720df74a26633ddfadc898b1e038ecec2
shannoncode <shannoncode@localhost.localdomain>
2016-03-18 16:27:40 -0400

    login token

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 var gulp  = require('gulp')
 var shell = require('gulp-shell')
 
-gulp.task('default',['steroids-login','connect'], function() {
+gulp.task('default',['connect','steroids-login','connect'], function() {
   // place code for your default task here
 });
 
 gulp.task('steroids-login', function(){ 
     return gulp.src('') .pipe(
         shell(
-                [ 'mkdir .appgyver', 'cp .appgyver/*.* ~/.appgyver', 'cp ~/.appgyver/*.* .appgyver', 'steroids login' ],
+                [ 'mkdir .appgyver', 'cp token.json ~/.appgyver', 'steroids login' ],
                 {ignoreErrors: true}
         )
      )
